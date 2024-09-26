e35e4fcdf2485081665bb7c786430faaf3f8348d
Altin <altintadayyon7@gmail.com>
2024-09-25 23:28:53 -0700

    Modified 'SonuclarFiles''s pro tip messages a little bit.

--- SonuclarFiles.cs
+++ SonuclarFiles.cs
@@ -17,27 +17,32 @@ namespace ADBoyaSU
         static string proTip = "\n\n\n" +
 
             "Yaxçi Bax 0: Yaxçı Bax 4'ə Yaxçı Bax\n" +
-            "Pro Tip 0: Pay Attention to Pro Tip 4\n\n\n" +
 
             "Yaxçı Bax 1: 'Ad' sutunundakı adların hammısı bir-birinə bənzəməsə," +
             "(tutak 'ceyran_1.png, taymaz_1.png, solmaz_1.png', ama birdən 'ayran_3.png, zaparat_3.png')" +
             "əkıslərin 3-dənə 3-dənə döül dimək olar.\n" +
 
+            "Yaxçı Bax 2: Yaxçı Bax.\n" +
+
+            "Yaxçı Bax 3: Bırada birzad yox. ama sən gənə Yaxçı Bax 1'ə Yaxçı Bax\n" +
+
+            "Yaxçı Bax 4: Yaxçı Bax 3'ə Yaxçı Bax\n\n" +
+            
+            
+            " -- + -------------------- + -- \n\n" +
+
+
+            "Pro Tip 0: Pay Attention to Pro Tip 4.\n" +
+
             "Pro Tip 1: If the names in the 'Ad' column do not look alike," +
             "(say, they're all like 'ceyran_1.png, taymaz_1.png, solmaz_1.png', but suddenly 'ayran_3.png, zaparat_3.png')" +
-            "there's a good chance your images are not triple-triple, something's wrong with them.\n\n\n" +
+            "there's a good chance your images are not triple-triple, something's wrong with them.\n" +
 
+            "Pro Tip 2: Pay Attention.\n" +
 
-            "Yaxçı Bax 2: Yaxçı Bax.\n" +
-            "Pro Tip 2: Pay Attention\n\n\n" +
-
-
-            "Yaxçı Bax 3: Yaxçı Bax 1'ə Yaxçı Bax\n" +
-            "Pro Tip 3: Pay Attention to Pro Tip 1\n\n\n" +
-
-
-            "Yaxçı Bax 4: Yaxçı Bax 3'ə Yaxçı Bax\n" +
-            "Pro Tip 4: Pay Attention to Pro Tip 3\n\n\n";
+            "Pro Tip 3: There's nothing here. But you still Pay Attention to Pro Tip 1 anyways.\n" +
+            
+            "Pro Tip 4: Pay Attention to Pro Tip 3.\n";
 
 
 
